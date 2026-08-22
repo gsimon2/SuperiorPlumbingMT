9ddef3b6a0788d09edd4ed8aca2b07fdf0eca22e
Glen Simon <glen.a.simon@gmail.com>
2026-08-22 07:37:34 -0400

    Add privacy policy page and link to bottom of page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 "use client"
-import { siteURL } from "@/content";
-import { Box, Typography } from "@mui/material";
+import { footerLinks, siteURL } from "@/content";
+import { Box, Link, Typography } from "@mui/material";
+import NextLink from "next/link";
 import React from "react";
 import ContactDisplay, { DisplayableContacts } from "./header/ContactDisplay";
 
@@ -20,9 +21,23 @@ const Footer: React.FC = () => {
             marginTop: "2rem",
          }}
       >
-         <Typography variant="h6" sx={{ textAlign: "center", mb: '0.5rem' }}>
-            {`Copyright ${currentDate.getFullYear()} | ${siteURL}`}
-         </Typography>
+         <Box sx={{ textAlign: "center", mb: "0.5rem" }}>
+            <Typography variant="h6">
+               {`Copyright ${currentDate.getFullYear()} | ${siteURL}`}
+            </Typography>
+            {footerLinks.map(({ title, url }) => (
+               <Link
+                  key={url}
+                  component={NextLink}
+                  href={url}
+                  color="secondary.main"
+                  variant="body2"
+                  sx={{ display: "inline-block", mt: 0.5 }}
+               >
+                  {title}
+               </Link>
+            ))}
+         </Box>
          <ContactDisplay
             iconColor="secondary.main"
             textColor="secondary.main"
